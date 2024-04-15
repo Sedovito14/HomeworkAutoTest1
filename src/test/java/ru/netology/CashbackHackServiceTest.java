a0b3079bd40c03d30508411c7c9110c9ca33f45e
Netotest <sakamotokenshin@yandex.ru>
2024-04-15 03:33:56 +0300

    New branch

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,26 @@
+package ru.netology;
+
+
+import static org.junit.Assert.assertEquals;
+
+
+public class CashbackHackServiceTest {
+    @org.junit.Test
+    public void testRemainNoCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void testRemainWithCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+        int actual = service.remain(amount);
+        int expected = 500;
+        assertEquals(actual, expected);
+    }
+
+}
